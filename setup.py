4800dc1560bb9866d0d2095738221a6a844841dd
AndiEcker <aecker2@gmail.com>
2026-03-28 14:16:59 +0000

    v0.3.9: enhanced project_name_guess() helper for namespace projects
    M  README.md
    M  aedev/project_vars.py
    M  setup.py
    M  tests/test_project_vars.py

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup_kwargs = {
     },
     'python_requires': '>=3.12',
     'url': 'https://gitlab.com/aedev-group/aedev_project_vars',
-    'version': '0.3.8',
+    'version': '0.3.9',
     'zip_safe': True,
 }
 
