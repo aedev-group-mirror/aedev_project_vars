fa979dea14c66c905771da44b220e67f3861f8fb
AndiEcker <aecker2@gmail.com>
2026-05-24 15:35:52 +0100

    v0.3.13: refactored to use new write_bin_file() helper from ae-base
    M  README.md
    M  aedev/project_vars.py
    M  setup.py
    M  tests/test_project_vars.py

--- aedev/project_vars.py
+++ aedev/project_vars.py
@@ -177,7 +177,7 @@ from aedev.commands import (                                                    
     editable_project_root_path, in_prj_dir_venv, git_remote_domain_group, git_remotes, git_tag_list)
 
 
-__version__ = '0.3.12'
+__version__ = '0.3.13'
 
 
 # PDV_* constants holding default values of all user/project specific configuration  ----------------------------------
--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup_kwargs = {
     },
     'python_requires': '>=3.12',
     'url': 'https://gitlab.com/aedev-group/aedev_project_vars',
-    'version': '0.3.12',
+    'version': '0.3.13',
     'zip_safe': True,
 }
 
